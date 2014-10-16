d75e19ab43872697282e283e5120541df60d0c69
jeangui <jg.lalanne@gmail.com>
2014-10-16 11:08:30 +0000

    initialization repo

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary: "Leaflet group layered control Plugin for Meteor ",
+  version: "1.0.0",
+  git: " \* Fill me in! *\ "
+});
+
+Package.onUse(function(api,where) {
+  api.versionsFrom('METEOR@0.9.3.1');
+  api.addFiles('lib/leaflet.groupedlayercontrol.js','client');
+  api.addFiles('style/leaflet.groupedlayercontrol.css','client');
+  api.addFiles('images/layers.png','client');
+});
+
+/*
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jeangui:leaflet.groupedlayercontrol');
+  api.addFiles('jeangui:leaflet.groupedlayercontrol-tests.js');
+});
+*/
